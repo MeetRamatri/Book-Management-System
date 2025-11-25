75735def596ecf174996c7687ebf89104f87d42d
MeetRamatri <meetramatri12@gmail.com>
2025-11-25 09:58:33 +0530

    Get Routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const prisma = require('./prisma.config');
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/books', (req, res) => {
-  res.send('List of books will be displayed here.');
+app.get('/books', async (req, res) => {
+  const books = await prisma.book.findMany();
+  res.json(books);
 });
 
-app.get('authors', (req, res) => {
-  res.send('List of authors will be displayed here.');
+app.get('/authors', async (req, res) => {
+  const authors = await prisma.author.findMany();
+  res.json(authors)
 });
 
-app.get('genres', (req, res) => {
-  res.send('List of genres will be displayed here.');
+app.get('/genres', async (req, res) => {
+  const genres = await prisma.genre.findMany();
+  res.json(genres)
 });
 
 app.listen(port, () => {
