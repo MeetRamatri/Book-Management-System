253ce49eb524aa9327f753551f77e302cee934e5
MeetRamatri <meetramatri12@gmail.com>
2025-11-26 09:48:53 +0530

    Routes

--- Backend/server.js
+++ Backend/server.js
@@ -42,6 +42,30 @@ app.get('/books', async (req, res) => {
   res.json(books);
 });
 
+app.post('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const book = await prisma.book.update({
+    where: { id: parseInt(id) },
+    data:{
+      title: body.title,
+      summary: body.summary,
+      isbn: body.isbn,
+      url: body.url
+    }
+  });
+  res.json(book);
+});
+
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const book = await prisma.book.deleteMany({
+    where: { id: parseInt(id) }
+  });
+  res.json(book);
+});
+
+
 app.get('/authors', async (req, res) => {
   const authors = await prisma.author.findMany();
   res.json(authors)
@@ -66,6 +90,32 @@ app.post('/authors', async (req, res) => {
   res.json(newAuthor);
 });
 
+app.post('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const author = await prisma.author.update({
+    where: { id: parseInt(id) },
+    data:{
+      first_name: body.first_name,
+      family_name: body.family_name,
+      date_of_birth: body.date_of_birth,
+      date_of_death: body.date_of_death,
+      name: body.name,
+      lifespan: body.lifespan,
+      url: body.url
+    }
+  });
+  res.json(author);
+});
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const author = await prisma.author.deleteMany({
+    where: { id: parseInt(id) }
+  });
+  res.json(author);
+});
+
 app.get('/genres', async (req, res) => {
   const genres = await prisma.genre.findMany();
   res.json(genres)
@@ -84,6 +134,28 @@ app.post('/genres', async (req, res) => {
   });
   res.json(newGenre);
 })
+
+app.post('/genres/:id', async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const genre = await prisma.genre.update({
+    where: { id: parseInt(id) },
+    data:{
+      name: body.name,
+      url: body.url
+    }
+  });
+  res.json(genre);
+});
+
+app.get('/genres/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const book = await prisma.genre.deleteMany({
+    where: { id: parseInt(id) }
+  });
+  res.json(book);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
